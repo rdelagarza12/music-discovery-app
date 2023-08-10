dd65faf05e8c043bf08b3febc4267fb26b921558
Rogelio De La Garza <rogeliodelagarza3901@gmail.com>
2023-08-09 23:35:45 -0400

    regenerated a secret key and created my env file and gitignore

--- back-end/library_app/models.py
+++ back-end/library_app/models.py
@@ -3,7 +3,7 @@ from user_app.models import User
 # Create your models here.
 class Library(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="library")
-    # playlist = models.ManyToManyField(Playlist, name="playlist")
+ 
 
 
     def __str__(self):
